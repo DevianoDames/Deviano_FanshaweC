8e8ac4445629b21cf5f90b52c748a39531cf82b2
DevianoDames <d_dames183654@fanshaweonline.ca>
2023-01-30 17:18:32 -0500

    made some changes

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 //use css selector to make connections to elements with JavaScript
-let theButton = document.querySelector("#buttonOne"),
+
+//create a 1 to 1 connection with a variable
+//let theButton = document.querySelector("#buttonOne"),
+
+//creat a 1 to many connection with a variable
+let theButton = document.querySelectorAll("#buttonHolder img"),
 	theHeading = document.querySelector("#headLine h1");
+	puzzleBoard = document.querySelector (".puzzle-board");
 // step 3
 //functionality always goes in the middle ---> how do we want
 //the app to behave?
 function changeBGImage () {
-	// debugger;
+	debugger;
 	theHeading.textContent = "Super Awesome Drag and Drop Game"
 	// start with the object, and then change a property
 	//or run a method
@@ -15,5 +21,10 @@ function changeBGImage () {
 //step 2
 //event handling always goes at the bottom =>
 //how do we want users to interact with our app
-theButton.addEventListener("click", changeBGImage);
+
+//1 to 1 event handling
+//theButton.addEventListener("click", changeBGImage);
+
+//1 to many even handling
+theButtons.forEach(button => button.addEventListener("click",changeBGImage));
 
