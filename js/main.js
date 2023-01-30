123310bb09430c9e56c51636fdfc73eb34bdff50
DevianoDames <d_dames183654@fanshaweonline.ca>
2023-01-30 16:52:03 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
-let theButton + document.querySelector("#buttoneOne"),theHeading = document.querySelector("#headLine h1");
+let theButton = document.querySelector("#buttoneOne"),
+theHeading = document.querySelector("#headLine h1");
 function changeBGImage () {
 	debugger;
+	theHeading.textContent = "Super Awesome Drag and Drop Game"
 }
+
+theHeading.classList.add ('oragne-heading');
